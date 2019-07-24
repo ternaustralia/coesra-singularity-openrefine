62be57262852087ab2ea044dd081313dadd585e3
hoangnguyen177 <hoangnguyen177@gmail.com>
2019-07-24 14:00:28 +1000

    fix build script

--- .travis/build-and-publish.sh
+++ .travis/build-and-publish.sh
@@ -2,7 +2,8 @@
 container_name=$1
 tag=$(git name-rev --name-only --tags HEAD)
 if [[ $tag != "undefined" ]]; then
-	/bin/bash .travis/build.sh -u "coesra/$container_name" -t $tag -c registry "Singularity.$container_name" 
+	latest_tag=$(git describe --abbrev=0)
+	/bin/bash .travis/build.sh -u "coesra/$container_name" -t $latest_tag -c registry "Singularity.$container_name" 
 else
 	echo "Not in tag. Not build attempted!"
 fi
