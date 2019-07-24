4fb7617db0f0c40702cd2374b0ea62d027317d24
hoangnguyen177 <hoangnguyen177@gmail.com>
2019-07-24 14:10:25 +1000

    remove tag

--- .travis/build-and-publish.sh
+++ .travis/build-and-publish.sh
@@ -5,7 +5,7 @@ if [[ $tag != "undefined" ]]; then
 	latest_tag=$(git describe --abbrev=0)
 	# make sure to build on the latest tag
 	git checkout $(latest_tag)
-	/bin/bash .travis/build.sh -u "coesra/$container_name" -t $latest_tag -c registry "Singularity.$container_name" 
+	/bin/bash .travis/build.sh -u "coesra/$container_name" -c registry "Singularity.$container_name" 
 else
 	echo "Not in tag. Not build attempted!"
 fi
