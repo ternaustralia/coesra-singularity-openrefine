c50292de030a2fd3185540651d8d5da3880c4ce2
hoangnguyen177 <hoangnguyen177@gmail.com>
2019-07-25 10:15:02 +1000

    small changes in firefox behaviour

--- startopenrefine.sh
+++ startopenrefine.sh
@@ -4,7 +4,7 @@
 REFINE_PID=$!
 sleep 2
 # open firefox to that address
-/usr/bin/firefox http://127.0.0.1:3333/ &
+/usr/bin/firefox --new-tab http://127.0.0.1:3333/ &
 FIREFOX_PID=$!
 wait $FIREFOX_PID
 kill -9 $REFINE_PID
