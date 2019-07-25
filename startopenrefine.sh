fa89b49db6fa17007275cc7c1f15d44c8829681d
hoangnguyen177 <hoangnguyen177@gmail.com>
2019-07-25 10:12:21 +1000

    kill webservice once firefox is done

--- startopenrefine.sh
+++ startopenrefine.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # start openrefine
 /opt/openrefine/refine &
+REFINE_PID=$!
 sleep 2
 # open firefox to that address
-/usr/bin/firefox http://127.0.0.1:3333/
+/usr/bin/firefox http://127.0.0.1:3333/ &
+FIREFOX_PID=$!
+wait $FIREFOX_PID
+kill -9 $REFINE_PID
